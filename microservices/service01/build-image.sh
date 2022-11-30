b88f939a5c7a812a469ec2cfe2f1a17e6ed172a0
ubuntu <ubuntu@STG-02.local>
2022-11-30 13:32:51 +0700

    add upload build serrvice

---
+++ microservices/service01/build-image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#remove images
+docker rmi $(docker images '10.19.2.92:5000/service01' -a -q)
+
+docker build --no-cache=true -t 10.19.2.92:5000/service01 .
+echo "Finish build docker images  "
+
+echo "Begin Push Images to Local Docker Repository"
+
+docker push "10.19.2.68:92/service01:latest"
+
+echo "Finish Push Images to Local Docker Repository"
+docker image ls
+
+exit 0
