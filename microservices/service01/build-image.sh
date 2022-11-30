3a3fe62e425ee18c8c41958faf09751b9729c3d0
root <root@JENKIN-SERVER.local>
2022-11-30 14:00:06 +0700

    fix build image

--- microservices/service01/build-image.sh
+++ microservices/service01/build-image.sh
@@ -8,7 +8,7 @@ echo "Finish build docker images  "
 
 echo "Begin Push Images to Local Docker Repository"
 
-docker push "10.19.2.68:92/service01:latest"
+docker push "10.19.2.92:5000/service01:latest"
 
 echo "Finish Push Images to Local Docker Repository"
 docker image ls
