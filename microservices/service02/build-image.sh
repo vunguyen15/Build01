222903c2fb9d316c80c296855b2ef6b6536a71bf
DUY TRAN <tdduy.dev@gmail.com>
2023-01-17 08:21:19 +0700

    add build jenkins service 01, service 02

--- microservices/service02/build-image.sh
+++ microservices/service02/build-image.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #remove images
-docker rmi $(docker images '192.168.56.2:32000/service01' -a -q)
+docker rmi $(docker images '192.168.56.2:32000/service02' -a -q)
 
-docker build --no-cache=true -t 192.168.56.2:32000/service01 .
+docker build --no-cache=true -t 192.168.56.2:32000/service02 .
 echo "Finish build docker images  "
 
 echo "Begin Push Images to Local Docker Repository"
 
-docker push "192.168.56.2:32000/service01:latest"
+docker push "192.168.56.2:32000/service02:latest"
 
 echo "Finish Push Images to Local Docker Repository"
 docker image ls
