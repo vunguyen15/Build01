86a6e72cc8e5b166af76c0a849934dd557da94fb
vunguyen15 <130273256+vunguyen15@users.noreply.github.com>
2023-04-09 15:25:37 +0700

    Update build-image.sh

--- microservices/service01/build-image.sh
+++ microservices/service01/build-image.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #remove images
-docker rmi $(docker images '192.168.56.2:32000/service01' -a -q)
+docker rmi $(docker images '192.168.10.7:32000/service01' -a -q)
 
-docker build --no-cache=true -t 192.168.56.2:32000/service01 .  -f ./microservices/service01/Dockerfile
+docker build --no-cache=true -t 192.168.10.7:32000/service01 .  -f ./microservices/service01/Dockerfile
 echo "Finish build docker images  "
 
 echo "Begin Push Images to Local Docker Repository"
 
-docker push "192.168.56.2:32000/service01:latest"
+docker push "192.168.10.7:32000/service01:latest"
 
 echo "Finish Push Images to Local Docker Repository"
 docker image ls
